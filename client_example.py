af497df90e7c503ca0d7fdb43e41af1b9f347cca
Jadiel <teofilojadiel@gmail.com>
2017-10-20 18:54:24 -0200

    Atualizar o cliente de exemplo para a chamar update e posteriormente chamar outro lookup

--- client_example.py
+++ client_example.py
@@ -1,13 +1,15 @@
 from AngraDBPythonDriver import Driver
 
-
 test_driver = Driver("127.0.0.1", 1234)
 response = test_driver.create_data_base("db")
 print response
 response = test_driver.connect_to_data_base("db")
 print response
-response = test_driver.save_document("document")
+doc_key = test_driver.save_document("document")
+print doc_key
+response = test_driver.lookup_document(doc_key)
 print response
-response = test_driver.lookup_document("0")
+response = test_driver.update_document(doc_key, "document_update")
 print response
-
+response = test_driver.lookup_document(doc_key)
+print response
